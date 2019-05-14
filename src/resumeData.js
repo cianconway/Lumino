92c83c9f6c35755b15071e1061f55e5d5083b28e
Cian Conway <cian.conway@outlook.com>
2019-05-14 22:41:56 +0100

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    imagebaseurl: "https://rbhatia46.github.io/",
+    name: "Cian Conway",
+    role: "Graphic Designer and Developer",
+    linkedinId: "Your LinkedIn Id",
+    skypeid: "Your skypeid",
+    roleDescription:
+        " I want to help you communicate who your company is through my branding and design strategy. I have five years design and development experience dealing with customers from fortune 500's to small businesses. Get in touch so I can help you.",
+    socialLinks: [
+        {
+            name: "linkedin",
+            url: "https://www.linkedin.com/in/cianconway/",
+            className: "fa fa-linkedin"
+        },
+        {
+            name: "github",
+            url: "http://github.com/cianconway",
+            className: "fa fa-github"
+        },
+        {
+            name: "skype",
+            url: "http://twitter.com/cianconway_",
+            className: "fa fa-twitter"
+        }
+    ],
+    aboutme:
+        "I am a designer with an engineering background, I have worked for startups for the past several years and made the decision to lead my own team. Graphic design and branding is the tool you use to communicate your companies value, it isn't just about a mark it is about showcasing what your company represents to your customers. Your brand has a direct impact on what your customers are willing to pay you, and we will help you do that. ",
+    address: "Clare",
+    website: "https://rbhatia46.github.io",
+    education: [
+        {
+            UniversityName: "The University of Limerick",
+            specialization: "Computer Engineering",
+            MonthOfPassing: "Aug",
+            YearOfPassing: "2014",
+            Achievements: "Some Achievements"
+        }
+    ],
+    work: [
+        {
+            CompanyName: "Some Company",
+            specialization: "Some specialization",
+            MonthOfLeaving: "Jan",
+            YearOfLeaving: "2018",
+            Achievements: "Some Achievements"
+        },
+        {
+            CompanyName: "Some Company",
+            specialization: "Some specialization",
+            MonthOfLeaving: "Jan",
+            YearOfLeaving: "2018",
+            Achievements: "Some Achievements"
+        }
+    ],
+    skillsDescription: "Your skills here",
+    skills: [
+        {
+            skillname: "HTML5"
+        },
+        {
+            skillname: "CSS"
+        },
+        {
+            skillname: "Reactjs"
+        }
+    ],
+    portfolio: [
+        {
+            name: "project1",
+            description: "mobileapp",
+            imgurl: "images/portfolio/phone.jpg"
+        },
+        {
+            name: "project2",
+            description: "mobileapp",
+            imgurl: "images/portfolio/project.jpg"
+        },
+        {
+            name: "project3",
+            description: "mobileapp",
+            imgurl: "images/portfolio/project2.png"
+        },
+        {
+            name: "project4",
+            description: "mobileapp",
+            imgurl: "images/portfolio/phone.jpg"
+        }
+    ],
+    testimonials: [
+        {
+            description: "This is a sample testimonial",
+            name: "Some technical guy"
+        },
+        {
+            description: "This is a sample testimonial",
+            name: "Some technical guy"
+        }
+    ]
+};
+
+export default resumeData;
